460d4b418b840e5c3814878a2c00978ecb9ab10a
s1250013 <s1250013@u-aizu.ac.jp>
2018-07-04 18:57:19 +0900

    Initial revision

---
+++ dice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+
+  int s1, s2, total=0;
+
+  /* 乱数の元を初期化 */
+  srand(time(NULL));
+
+  printf("Rolling the dice...\n");
+
+  s1 = rand()% 6+1;
+  printf("Dice 1: %d\n", s1);
+  
+  s2 = rand()% 6+1;
+  printf("Dice 2: %d\n", s2);
+
+  total = s1 + s2;
+  printf("Total value: %d\n", total);
+
+  
+  return 0;
+}
