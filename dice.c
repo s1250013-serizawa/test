86c931d02e58f98ef0ff208585b174b5b5c7fc20
s1250013 <s1250013@u-aizu.ac.jp>
2018-07-04 19:03:26 +0900

    Won/lost message added

--- dice.c
+++ dice.c
@@ -20,6 +20,8 @@ int main(void){
   total = s1 + s2;
   printf("Total value: %d\n", total);
 
+  if(total >= 7) printf("You won!\n");
+  else if(total < 7) printf("You lost!\n");
   
   return 0;
 }
